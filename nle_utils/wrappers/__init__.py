c9ce7f78cca64e30071e4f0fd64616f13189b7e3
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-07-09 13:50:26 +0200

    add more wrappers

--- nle_utils/wrappers/__init__.py
+++ nle_utils/wrappers/__init__.py
@@ -2,16 +2,22 @@ from nle_utils.wrappers.blstats_info import BlstatsInfoWrapper
 from nle_utils.wrappers.final_stats_info import FinalStatsWrapper
 from nle_utils.wrappers.last_info import LastInfo
 from nle_utils.wrappers.nle_demo import NLEDemo
+from nle_utils.wrappers.nle_timelimit import NLETimeLimit
+from nle_utils.wrappers.prev_actions import PrevActionsWrapper
+from nle_utils.wrappers.render_char_images import RenderCharImagesWithNumpyWrapperV2
 from nle_utils.wrappers.render_tiles import RenderTiles
 from nle_utils.wrappers.task_rewards_info import TaskRewardsInfoWrapper
 from nle_utils.wrappers.ttyrec_info import TtyrecInfoWrapper
 
 __all__ = [
+    BlstatsInfoWrapper,
+    FinalStatsWrapper,
+    LastInfo,
     NLEDemo,
+    PrevActionsWrapper,
+    RenderCharImagesWithNumpyWrapperV2,
     RenderTiles,
     TaskRewardsInfoWrapper,
-    BlstatsInfoWrapper,
-    LastInfo,
-    FinalStatsWrapper,
+    NLETimeLimit,
     TtyrecInfoWrapper,
 ]
